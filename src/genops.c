1f8b7ca8180522f3982ccf88d608702f4bbbd2f5
Eoin Murphy <eoin@murph.ie>
2011-07-27 10:06:10 +0100

    untested one point crossover

--- src/genops.c
+++ src/genops.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "confuse.h"
 
@@ -14,18 +15,30 @@ void intflipMutation(Genotype *individual) {
 }
 
 void onepointCrossover(Genotype *parent, Genotype *spouse) {
+  /* Only crossover with a certain probability */
   if ((float)rand()/(float)RAND_MAX > cfg_getfloat(config, "crossoverProb"))
     return;
 
+  /* Get xo points - TODO get points from inside encoding region */
   int parentPoint = rand()%parent->length;
   int spousePoint = rand()%spouse->length;
 
-  if (parentPoint + (spouse->length-spousePoint) > parentPoint->maxLength) {
-    parent->maxLength = parentPoint + (spouse->length-spousePoint);
+  int parentLength = parent->length - parentPoint;
+  int spouseLength = spouse->length - spousePoint;
+
+  /* Increase chromosome capacity if required */
+  if (parentPoint +  spouseLength > parent->maxLength) {
+    parent->maxLength = parentPoint + spouseLength;
     realloc(parent->codons, parent->maxLength*sizeof(unsigned int));
   }
-  if (spousePoint + (parent->length-parentPoint) > spousePoint->maxLength) {
-    spouse->maxLength = spousePoint + (parent->length-parentPoint);
+  if (spousePoint + parentLength > spouse->maxLength) {
+    spouse->maxLength = spousePoint + parentLength;
     realloc(spouse->codons, spouse->maxLength*sizeof(unsigned int));
   }
+
+  /* Simple temp buffer copying */
+  unsigned int *temp;
+  memcpy(temp, (parent->codons + parentPoint), parentLength*sizeof(unsigned int));
+  memcpy((parent->codons + parentPoint), (spouse->codons + spousePoint), spouseLength*sizeof(unsigned int));
+  memcpy((spouse->codons + spousePoint), temp, parentLength*sizeof(unsigned int));
 }
